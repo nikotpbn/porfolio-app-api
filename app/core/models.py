cfc8f2f9a089fe0614860325627aea39a0e9d4cf
Nicolau Tolentino Pamplona Beltrão Neto <niko@nicolaus-mbp.lan>
2023-03-11 09:40:41 +0000

    User model and tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,53 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
 # Create your models here.
+class MyUserManager(BaseUserManager):
+    """Manager for custom User"""
+
+    def create_user(self, email, password=None, **kwargs):
+        """
+        Creates and saves a User with the given email
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom User"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
