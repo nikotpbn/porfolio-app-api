9d320cbf979dc9c7a22eb010a7906e5ed4e4dacf
Nicolau Tolentino Pamplona Beltrão Neto <niko@Nicolaus-MacBook-Pro.local>
2023-08-07 14:42:17 +0100

    search to admin page

--- app/portfolio/admin.py
+++ app/portfolio/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from portfolio import models
 
-admin.site.register(models.Character)
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class CharacterAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(models.Character, CharacterAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Artist)
+admin.site.register(models.Artist, ArtistAdmin)
 admin.site.register(models.Art)
